8fc7527dd4ba4115f4377a9a83df2c2109d585dc
Franco Mariotti <fmariotti@fi.uba.ar>
2021-01-28 18:12:02 -0300

    correccion ejercicio 6

--- ejercicio6_final2012.c
+++ ejercicio6_final2012.c
@@ -1,6 +1,7 @@
 /* Escribir un aplicacion C que modifique el archivo a.bin triplicando (repitiendo 3 veces) los bytes mayores a 0xD1.
 */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
@@ -8,30 +9,38 @@
 #define FILENAME "a.bin"
 #define MAX_BYTE 0xD1
 
+int fileno(FILE* stream);
+int ftruncate(int fd,off_t length);
+
 int main(int argc, char const *argv[]) {
     FILE* frd = fopen(FILENAME,"rb");
     FILE* fwr = fopen(FILENAME,"r+b");
 
     fseek(fwr,0,SEEK_END);
     int len = ftell(fwr);
-    char c=0;
+    int bytes = 0;
+    unsigned char c=0;
 
     for (int i = 0; i < len; i++) {
     	c = fgetc(frd);
 
     	if (c > MAX_BYTE) {
-    		fwrite(&c,sizeof(char),3,fwr);
+		putc(c,fwr);
+		putc(c,fwr);
+		putc(c,fwr);
+		bytes+=3;
     	} else {
-    		putc(c,wr);
+    		putc(c,fwr);
+    		bytes++;
     	}
     }
 
     fseek(fwr,0,SEEK_SET);
 
-    while (c!=EOF) {
+    for (int i = 0; i < bytes; i++) {
     	c = fgetc(frd);
-    	putc(c,fwr); 
-    } 
+    	putc(c,fwr);
+    }
 
     ftruncate(fileno(fwr),ftell(fwr));
 
